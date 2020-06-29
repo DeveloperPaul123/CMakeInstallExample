17d75efda25f74135e582085d23f033694ad4e93
Paul Tsouchlos <developer.paul.123@gmail.com>
2020-06-29 12:39:46 -0400

    Project Updates for Newer Versions of CMake

--- cmake/CpackConfig.cmake
+++ cmake/CpackConfig.cmake
@@ -1,31 +1,42 @@
-#installer rules. 
+# installer rules. 
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Example cmake installation.")
-set(CPACK_PACKAGE_VENDOR "My Fake Companay, Inc.")
+set(CPACK_PACKAGE_VENDOR "DeveloperPaul123")
 set(CPACK_PACKAGE_VERSION ${PROJECT_VER})
 set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VER_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VER_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VER_PATCH})
 set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL OFF)
 set(CPACK_NSIS_MODIFY_PATH ON)
-#if you have an icon set the path here 
+
+# if you have an icon set the path here 
 # SET(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}/my_cool_icon.ico")
-set(CPACK_PACKAGE_INSTALL_DIRECTORY "MyFakeCompany\\\\CMakeInstallExample")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "DeveloperPaul123\\\\CMakeInstallExample")
 set(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\CMakeInstallExample.exe")
 set(CPACK_NSIS_DISPLAY_NAME "CMakeInstallExample ${PROJECT_VER}")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/EULA.txt")
 
-# set component installation rules 
-set(CPACK_COMPONENT_BINARIES_DISPLAY_NAME "Main application")
-set(CPACK_COMPONENT_BINARIES_DESCRIPTION "This will install the main application.")
-set(CPACK_COMPONENT_DATA_FILES_DISPLAY_NAME "Data files for this fake application.")
-set(CPACK_COMPONENT_DATA_FILES_DESCRIPTION "This will install random data files.")
-set(CPACK_ALL_INSTALL_TYPES Full Upgrade) #set installation types
-set(CPACK_COMPONENT_BINARIES_INSTALL_TYPES Full Upgrade)
-set(CPACK_COMPONENT_DATA_FILES_INSTALL_TYPES Full)
+include(CPackComponent)
+
+cpack_add_install_type(Full DISPLAY_NAME "Install Everything")
+cpack_add_install_type(Upgrade DISPLAY_NAME "Upgrade Only")
+
+cpack_add_component(binaries
+    DISPLAY_NAME "Main application"
+    DESCRIPTION "This will install the main application."
+    REQUIRED
+    INSTALL_TYPES Full Upgrade
+)
+
+cpack_add_component(data_files
+    DISPLAY_NAME "Data files"
+    DESCRIPTION "Fake data files for this example."
+    INSTALL_TYPES Full
+)
+
 set(CPACK_COMPONENTS_ALL binaries data_files)
 if (CMAKE_CL_64)
-	set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+    set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
 else (CMAKE_CL_64)
-	set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
+    set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
 endif (CMAKE_CL_64)
 include(CPack)
